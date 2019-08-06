cb37d79ae4fad529b8143718146a41371d748b1a
Student <ziwei.polaris@gmail.com>
2019-08-06 14:46:13 +0100

    tests added

--- J-Zi/NoteZi.js
+++ J-Zi/NoteZi.js
@@ -1,8 +1,14 @@
-
-
 describe('Note', function(){
-  it("it block working",function(){
-    assert.isTrue(true);
-  })
-});
-// Our describe block will check if our test it reaching our notes array.
+ it("it block working",function(){
+   assert.isTrue(true);
+ })
+ var note = "Don't cry because it's over, smile because it happened. I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."
+ it('adds a full length note', function() {
+   Napp.add(note);
+   assert.isTrue((Napp.notes[0]) == note);
+ }),
+ it('returns the preview state of note', function() {
+   Napp.add(note);
+   assert.isTrue((Napp.all()[0]).length == 20);
+ });
+})
