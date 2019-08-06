dbe5b71e7df22fc748b79767fcf0fd3f28bbfa7f
Kaja <catcalledkaja@gmail.com>
2019-08-06 14:32:12 +0100

    Adding Event Listener

--- Lit/Note.js
+++ Lit/Note.js
@@ -6,21 +6,34 @@ console.log("Working file")
 //Note
 // Notepreview
 
+
 const napp = {
   notes: [],
   //notes: ["The quick brown fox jumps over the lazy dog is an English-language pangram—a sentence that contains all of the letters of the alphabet. It is commonly used for touch-typing practice, testing typewriters and compute","The earliest known appearance of the phrase is from The Boston Journal. In an article titled Current Notes in the February 10, 1885, morning edition, the phrase is mentioned as a good practice sentence for writing students"],
   all: function(){
-    return napp.notes.map(note => note.slice(0, 19));
+    return napp.notes.map(note => note.slice(0, 20));
   },
 
   add: function(note) {
     napp.notes.push(note)
+  },
+  init: function(){
+    const create = document.getElementById('create')
+    create.addEventListener('click', function(event){
+      event.preventDefault();
+      let new_note = document.getElementById('textarea').value;
+
+      console.log(new_note)
+    })
   }
+
 }
+
+document.addEventListener('DOMContentLoaded', napp.init);
 
 
 // console.log(napp.notes);
 
 // napp.add("hellokjasldkjdalkjalkdjaflkjfalkfjalkfjalfjlfjflajalfkj")
-// // 
+// //
 // console.log(napp.all());
