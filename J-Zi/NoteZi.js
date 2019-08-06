0c8dafa388c57b6996fdbb616555e7a7c3493949
Student <ziwei.polaris@gmail.com>
2019-08-06 15:15:24 +0100

    div classes added to html

--- J-Zi/NoteZi.js
+++ J-Zi/NoteZi.js
@@ -4,11 +4,11 @@ describe('Note', function(){
  })
  var note = "Don't cry because it's over, smile because it happened. I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."
  it('adds a full length note', function() {
-   Napp.add(note);
-   assert.isTrue((Napp.notes[0]) == note);
+   napp.add(note);
+   assert.isTrue((napp.notes[0]) == note);
  }),
  it('returns the preview state of note', function() {
-   Napp.add(note);
-   assert.isTrue((Napp.all()[0]).length == 20);
+   napp.add(note);
+   assert.isTrue((napp.all()[0]).length == 20);
  });
 })
