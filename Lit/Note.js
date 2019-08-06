63970888eccee40fe0347445271d17702cadb886
Student <student@Makerss-MacBook-Pro.local>
2019-08-06 14:33:40 +0100

    tests for add and all methods

--- Lit/Note.js
+++ Lit/Note.js
@@ -1,1 +1,47 @@
 console.log("Working file")
+
+
+const Napp = {
+
+  notes: [],
+
+  all: function () {
+
+    return Napp.notes.map(quote => quote.slice(0,20))
+},
+
+  add: function(input) {
+  Napp.notes.push(input)
+
+}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// function Note() {
+//   this.notes = [];
+//
+// }
+// Note.prototype.add = function (message) {
+//   this.notes.push(message);
+//
+// };
+//
+// Note.prototype.all = function() {
+//   return this.notes;
+// }
+//
+// Note.prototype.find = function(index) {
+//
+// }
