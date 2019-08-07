4a38a090259ce7873615e76976a6736162756c51
Student <ziwei.polaris@gmail.com>
2019-08-07 17:16:54 +0100

    BuuuBBBLLELLEEEEssssS

--- Lit/Note.js
+++ Lit/Note.js
@@ -28,9 +28,11 @@ const napp = {
       if (id == undefined) {      // if id is undefined, we change the current page to hidden, and make the hidden note visible
         document.getElementById('page2').classList = "inactive";  // id if undefined only when we want to go back to home page, so we want to hide the hidden note whenver this happens.
         document.getElementById('page').classList = "active";
+        killbubbles()
       } else {
         hidePage();
         showFullNote(id);
+        yaybubbles()
       }
     })
 
@@ -59,6 +61,7 @@ const napp = {
       napp.add(new_note)
       document.getElementById('textarea').value = "" // empties text area
       napp.show_notes()
+      playSound()
       // napp.add_expanded_notes()
       // napp.add_links()
 
@@ -102,3 +105,89 @@ document.addEventListener('DOMContentLoaded', napp.init);
 // napp.add("hellokjasldkjdalkjalkdjaflkjfalkfjalkfjalfjlfjflajalfkj")
 // //
 // console.log(napp.all());
+function yaybubbles() {
+			// Note the screen size.
+			var screenSize = {
+				height : $(document).height(),
+				width : $(document).width()
+			};
+
+			// Generate random bubbles on screen.
+			for (var i = 0; i < 500; i++) {
+				// Random location and size.
+				var size = Math.random() * 40 + 20,
+					left = screenSize.width * Math.random(),
+					top = screenSize.height * Math.random();
+
+				// Add the bubble to the page.
+				var bubble = $("<div>", {
+					"class" : "Bubble",
+					css : {
+						left : left,
+						top : top,
+						height : size,
+						width : size,
+						margin : "-" + (size / 2) + "px"
+					}
+				}).appendTo(document.body).get(0);
+
+				// Record this bubble's info into the element itself for easier access.
+				bubble.radius = size / 2;
+			}
+
+			// Retain the bubbles in memory as a jQuery object, for quicker access.
+			var bubbles = $(".Bubble");
+
+			// User's cursor bubble.
+			var userBubble = $("<div>", {
+				id : "UserBubble",
+				css : {
+					left : -100,
+					top : -100
+				}
+			}).appendTo(document.body);
+
+			// Note the user bubble's size.
+			var userBubbleRadius = userBubble.width() / 2;
+
+			// Page mouse move function...
+			$(document).mousemove(function(event) {
+				// Move the user bubble.
+				userBubble.css({
+					left : event.clientX,
+					top : event.clientY
+				});
+
+				// Check each bubble to see if it's overlapped by the user bubble.
+				bubbles.each(function() {
+					var pos = $(this).offset();
+
+					// Calculate distance between the user bubble and this bubble.
+					var distance = Math.sqrt(Math.pow(pos.left + this.radius - event.clientX, 2) +
+						Math.pow(pos.top + this.radius - event.clientY, 2)),
+
+						overlap = userBubbleRadius + this.radius - distance;
+
+					// If the bubbles overlap...
+					if (overlap > 0) {
+						var xMovement = (pos.left - event.clientX) * distance / 20,
+							yMovement = (pos.top - event.clientY) * distance / 20;
+
+						$(this).stop().animate({
+							left : (xMovement > 0 ? "+" : "-") + "=" + (xMovement > 0 ? xMovement : -xMovement),
+							top : (yMovement > 0 ? "+" : "-") + "=" + (yMovement > 0 ? yMovement : -yMovement)
+						}, 500);
+					}
+				});
+			});
+		};
+
+  function killbubbles() {
+    $(".Bubble").remove()
+    $("#UserBubble").remove()
+  }
+
+  function playSound() {
+    const audio = document.getElementById("audio")
+    audio.play()
+  }
